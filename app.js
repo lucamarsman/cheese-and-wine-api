d234007825bd91cc5682c7b6c0cb1017a3f211d8
chunk01 <lucamarsman@live.com>
2025-01-20 17:29:07 -0500

    Implement POST method on cheeses route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cheesesController from './controllers/cheeses.js';
 
 // Create expess server object
 const app = express();
+app.use(bodyParser.json());
 
 // Controllers
-app.use('/cheeses', cheesesController);
+app.use('/api/v1/cheeses', cheesesController);
 
 // Start web server
 app.listen(3000, () => {
--- controllers/cheeses.js
+++ controllers/cheeses.js
@@ -15,5 +15,20 @@ router.get('/', (req, res) => {
     return res.status(200).json(cheeses);
 });
 
+// GET: {id} return selected cheese by id
+router.get('/:id', (req, res) => {
+    let index = cheeses.findIndex(c => c.id == req.params.id);
+    if(index == -1){
+        return res.status(404).json({msg: 'Not Found'});
+    }
+    return res.status(200).json(cheeses[index]);
+});
+
+// POST: add new cheese from POST body
+router.post('/', (req, res) => {
+    cheeses.push(req.body);
+    return res.status(201).json(); // 201: resource created
+});
+
 // make controller public to rest of the app
 export default router;
