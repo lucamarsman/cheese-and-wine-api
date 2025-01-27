2318f561be81cda19afe757663467aa0b7091673
chunk01 <lucamarsman@live.com>
2025-01-27 16:31:13 -0500

    Add swagger ui libraries and YAML comments

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cheesesController from './controllers/cheeses.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from "swagger-ui-express";
 
 // Create expess server object
 const app = express();
 app.use(bodyParser.json());
+
+// swagger config
+const docOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Cheese API',
+            version: '1.0.0'
+        }
+    },
+    apis: ['./controllers/*.js'] // where to find api methods (controllers)
+};
+const openapiSpecification = swaggerJSDoc(docOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 // Controllers
 app.use('/api/v1/cheeses', cheesesController);
--- controllers/cheeses.js
+++ controllers/cheeses.js
@@ -10,12 +10,37 @@ let cheeses = [
     { id: 3, name: "Leicester" }
 ];
 
-// GET: return all cheeses
+/**
+ * @swagger
+ * /api/v1/cheeses:
+ *   get:
+ *     summary: Retrieve all cheeses
+ *     responses:
+ *       200:
+ *         description: A list of cheeses
+ */
 router.get('/', (req, res) => {
     return res.status(200).json(cheeses);
 });
 
-// GET: {id} return selected cheese by id
+
+/**
+ * @swagger
+ * /api/v1/cheeses/{id}:
+ *   get:
+ *     summary: Find cheese by its id
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         schema:
+ *           type: integer
+ *           required: true
+ *     responses:
+ *       200:
+ *         description: Returns a single cheese
+ *       404:
+ *         description: Not found
+ */
 router.get('/:id', (req, res) => {
     let index = cheeses.findIndex(c => c.id == req.params.id);
     if(index == -1){
@@ -24,12 +49,63 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(cheeses[index]);
 });
 
-// POST: add new cheese from POST body
+/**
+ * @swagger
+ * /api/v1/cheeses:
+ *   post:
+ *     summary: add new cheese from POST body
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *               name:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Resource created
+ *       400:
+ *         description: Bad request
+ */
 router.post('/', (req, res) => {
     cheeses.push(req.body);
     return res.status(201).json(); // 201: resource created
 });
 
+/**
+ * @swagger
+ * /api/v1/cheeses/{id}:
+ *   put:
+ *     summary: update selected cheese from request body
+ *     parameters:
+ *       -name: id
+ *       in: path
+ *       required: true
+ *       schema:
+ *         type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *               name:
+ *                 type: string
+ *     responses:
+ *       204:
+ *         description: Resource updated
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Not found
+ */
 router.put('/:id', (req, res) => {
     let index = cheeses.findIndex(c => c.id == req.params.id);
     if(index == -1){
@@ -39,6 +115,23 @@ router.put('/:id', (req, res) => {
     res.status(204).json(); // 204: resource modified
 });
 
+/**
+ * @swagger
+ * /api/v1/cheeses/{id}:
+ *   delete:
+ *     summary: Remove selected cheese
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         schema:
+ *           type: integer
+ *           required: true
+ *     responses:
+ *       204:
+ *         description: Resource updated (removed)
+ *       404:
+ *         description: Not found
+ */
 router.delete('/:id', (req, res) => {
     let index = cheeses.findIndex(c => c.id == req.params.id);
     if(index == -1){
